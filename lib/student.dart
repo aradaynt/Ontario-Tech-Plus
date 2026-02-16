23385a6c36d2025ddf5bfec2911c12df133dfa4a
Arad Ayntabli <arad.ayntabli@ontariotechu.net>
2026-02-15 22:00:56 -0500

    Added Constructors to Student, Course, and Instructor class

--- lib/student.dart
+++ lib/student.dart
@@ -5,6 +5,21 @@ class Student {
   String faculty = '';
   int year = 0;
   List<Course> courses = [];
+  Student(this.name, this.studentid, this.program, this.faculty, this.year);
+  void addCourse(Course course) {
+    courses.add(course);
+  }
+
+  void removeCourse(Course course) {
+    courses.remove(course);
+  }
+
+  void editCourse(Course course) {
+    int index = courses.indexOf(course);
+    if (index != -1) {
+      courses[index] = course;
+    }
+  }
 }
 
 enum SessionType { lecture, lab, tutorial }
@@ -17,6 +32,15 @@ class Course {
   String location = '';
   Instructor? instructor;
   SessionType sessiontype = SessionType.lecture;
+  Course(
+    this.name,
+    this.code,
+    this.whattime,
+    this.whatdays,
+    this.location,
+    this.instructor,
+    this.sessiontype,
+  );
 }
 
 class Instructor {
@@ -24,4 +48,5 @@ class Instructor {
   String email = '';
   String office = '';
   DateTime? officehours;
+  Instructor(this.name, this.email, this.office, this.officehours);
 }
