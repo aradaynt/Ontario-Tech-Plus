fd7464406b42f4c2a970556a2e22ef7c74de9930
Aaron <aaron.james2@ontariotechu.net>
2026-02-24 23:14:45 -0500

    first iteration profile mostly complete

--- lib/core/global_providers/user_provider.dart
+++ lib/core/global_providers/user_provider.dart
@@ -1,13 +1,10 @@
 // OntarioTechPlus - user_provider.dart
 
 // Gets the current user
-// Temporarily grabs profile data. Will have proper profile provider later with a proper model rather than map.
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
 import 'package:ontario_tech_plus/auth/auth_providers.dart';
-import 'package:ontario_tech_plus/core/global_providers/supabase_provider.dart';
 
 // Provides the currently signed-in supabase user.
 // Will return null if signed out, still loading or error occures
@@ -18,30 +15,4 @@ final currentUserProvider = Provider<User?>((ref) {
     data: (session) => session?.user,
     orElse: () => null,
   );
-});
-
-// Fet the user profile from the profiles table (Only used as a demonstration for home page, will use proper model later)
-final profileProvider = FutureProvider.autoDispose<Map<String, dynamic>?>((
-  ref,
-) async {
-  final user = ref.watch(currentUserProvider); // Get the current user
-
-  // If no user, no profile
-  if (user == null) return null;
-
-  final supabase = ref.watch(supabaseProvider); //Get supabase through provider
-
-  // Try to fetch the profile table data
-  try {
-    final row = await supabase
-        .from('profiles')
-        .select('firstname, lastname, email, student_number')
-        .eq('id', user.id)
-        .single();
-
-    return row;
-  } catch (_) {
-    // Return null if profile doesnt exist
-    return null;
-  }
 });
