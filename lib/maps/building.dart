b527598ac3ffff56525dd1797dd228e4566add70
Ayaan-Mustafa <ayaan.mustafa@ontariotechu.net>
2026-02-25 14:08:29 -0500

    Added routing to the map with polyline

--- lib/maps/building.dart
+++ lib/maps/building.dart
@@ -1,10 +1,9 @@
 import 'package:latlong2/latlong.dart';
-import 'package:flutter_map/flutter_map.dart';
 
 class Building {
   final String name;
   final LatLng centre;
-  final Polygon polygon;
+  final List<LatLng> polygon;
 
   Building(this.name, this.centre, this.polygon);
 }
